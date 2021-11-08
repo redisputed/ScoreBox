8e2573296e7385ad88fbb9afcf12f6b10c5ebab7
jpd <redisputed@gmail.com>
2021-11-07 20:33:17 -0800

    start the DB build using postgres

--- main.py
+++ main.py
@@ -2,12 +2,18 @@
 from kivy.uix.pagelayout import PageLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 from kivy.app import App
 import kivy
+from kivy.uix.widget import Widget
 kivy.require('2.0.0')  # replace with your current kivy version !
 
 
-class ScoreBox(PageLayout):
+class gameInfo(GridLayout):
+    pass
+
+
+class ScoreBox(Widget):
     pass
 
 
